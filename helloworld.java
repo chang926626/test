263a6de93e019a7c9ff0d3fd0aee86f2423c98b9
chang926626 <68771873+chang926626@users.noreply.github.com>
2020-07-25 18:06:48 +0800

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
 public class helloworld{
 	public static void main(args[]){
 		System.out.println("helloworld")
-
+		System.out.println("2020年7月25日18:06:39")
+		System.out.println("helloworld")
 	}
 }
